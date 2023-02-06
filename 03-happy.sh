8d18af9444c88c9fc8b1dd1a02ffc419a8b161b8
Elleni <ellu.miru@gmail.com>
2023-02-05 22:23:15 -0500

    Added on happy

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,12 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+echo "Is it the weekend?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Happy weekend."
+else
+   echo "Happy weekday."
+
